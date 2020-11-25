4ac9e318cc69e9d1430bd524e820351dda3bcf95
mullonch <48211643+mullonch@users.noreply.github.com>
2020-11-25 17:31:06 +0100

    Changement du VLDM

--- Openstack_configuration/VLDM1-openrc.sh
+++ Openstack_configuration/VLDM1-openrc.sh
@@ -26,10 +26,9 @@ unset OS_TENANT_NAME
 # performing the action as the **user**.
 export OS_USERNAME="mullor"
 # With Keystone you pass the keystone password.
-#echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
-#read -sr OS_PASSWORD_INPUT
-#export OS_PASSWORD=$OS_PASSWORD_INPUT
-export OS_PASSWORD=yFFvvyTjs2wVz7q
+echo "Please enter your OpenStack Password for project $OS_PROJECT_NAME as user $OS_USERNAME: "
+read -sr OS_PASSWORD_INPUT
+export OS_PASSWORD=$OS_PASSWORD_INPUT
 # If your configuration has multiple regions, we set that information here.
 # OS_REGION_NAME is optional and only valid in certain environments.
 export OS_REGION_NAME="RegionINSA"
